456c3376c57a331f7abe2e0f16ef2740e57e8ad4
yrk721 <yoorae0721@naver.com>
2023-03-28 20:36:29 +0900

    [OUR-2] pages 재구성

--- client/pages/menu/index.tsx
+++ client/pages/menu/index.tsx
@@ -1,7 +1,27 @@
+// 컴포넌트
+import BackButton from "../../src/components/commons/BackButton";
+import Signature from "../../src/components/category/Signature";
+import Burgers from "../../src/components/category/Burgers";
+import SetMenu from "../../src/components/category/SetMenu";
+import SideMenu from "../../src/components/category/SideMenu";
+import Drink from "../../src/components/category/Drink";
+import Desserts from "../../src/components/category/Desserts";
+import CartList from "../../src/components/commons/layout/cart/CartList.container";
+
 export default function MenuPage() {
   return (
     <>
-      <div>MenuPage</div>
+      <BackButton />
+      <div>SelectCategoryPage</div>
+      <div>
+        <Signature />
+        <Burgers />
+        <SetMenu />
+        <SideMenu />
+        <Drink />
+        <Desserts />
+      </div>
+      <CartList />
     </>
   );
 }
