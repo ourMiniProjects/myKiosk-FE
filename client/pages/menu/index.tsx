5965f1b0d7101fe3315f31c436c743dfd63a46f8
yrk721 <yoorae0721@naver.com>
2023-03-29 11:32:14 +0900

    [OUR-13] menu page UI 구성 - 장바구니, header 추가

--- client/pages/menu/index.tsx
+++ client/pages/menu/index.tsx
@@ -1,4 +1,4 @@
-// 컴포넌트
+//컴포넌트;
 
 import Signature from "../../src/components/units/category/Signature";
 import Burgers from "../../src/components/units/category/Burgers";
@@ -6,23 +6,51 @@ import SetMenu from "../../src/components/units/category/SetMenu";
 import SideMenu from "../../src/components/units/category/SideMenu";
 import Drink from "../../src/components/units/category/Drink";
 import Desserts from "../../src/components/units/category/Desserts";
-import CartList from "../../src/components/commons/cart/CartList.container";
 import BackButton from "../../src/components/commons/button/BackButton";
+import CartList from "../../src/components/commons/cart/CartList.container";
+import styled from "styled-components";
+import LayoutHeader from "../../src/components/commons/layout/header/LayoutHeader.container";
 
 export default function MenuPage() {
   return (
     <>
-      <BackButton />
-      <div>MenuPage</div>
-      <div>
-        <Signature />
-        <Burgers />
-        <SetMenu />
-        <SideMenu />
-        <Drink />
-        <Desserts />
-      </div>
-      <CartList />
+      <Wrapper>
+        <div style={{ width: "75%" }}>
+          <LayoutHeader />
+          <Contents>
+            <BackButton />
+            <div>MenuPage</div>
+            <div>
+              <Signature />
+              <Burgers />
+              <SetMenu />
+              <SideMenu />
+              <Drink />
+              <Desserts />
+            </div>
+          </Contents>
+        </div>
+        <div style={{ width: "25%" }}>
+          <CartList />
+        </div>
+      </Wrapper>
     </>
   );
 }
+
+// style : 임시
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Contents = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 50px;
+  margin: 20px;
+  border: solid 2px grey;
+`;
