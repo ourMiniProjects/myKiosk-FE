599f71f1aede414aef268756b5c06c989881fc0d
yrk721 <yoorae0721@naver.com>
2023-03-29 10:34:19 +0900

    파일 경로 변경

--- client/pages/menu/index.tsx
+++ client/pages/menu/index.tsx
@@ -6,7 +6,7 @@ import SetMenu from "../../src/components/units/category/SetMenu";
 import SideMenu from "../../src/components/units/category/SideMenu";
 import Drink from "../../src/components/units/category/Drink";
 import Desserts from "../../src/components/units/category/Desserts";
-import CartList from "../../src/components/commons/layout/cart/CartList.container";
+import CartList from "../../src/components/commons/cart/CartList.container";
 import BackButton from "../../src/components/commons/button/BackButton";
 
 export default function MenuPage() {
