f8428b67ac64b1e41c61c581a05e749ac602c3c2
Jiseok Kim <86154028+p1ants@users.noreply.github.com>
2023-03-29 11:13:19 +0900

    [OUR-16] db.json - axios 통신 , 데이터 타입설정

--- client/src/components/units/category/Burgers.tsx
+++ client/src/components/units/category/Burgers.tsx
@@ -1,3 +1,52 @@
-export default function Burgers() {
-  return <div>Burgers</div>;
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+interface MenuItem {
+  id: number;
+  menuName: string;
+  price: number;
+  imageUrl: string;
+  amount: number;
 }
+
+type MenuList = MenuItem[][];
+
+const fetchMenuList = async () => {
+  const response = await axios.get('http://localhost:3001/MenuList');
+  const menuList = response.data as MenuList;
+  return menuList;
+};
+
+const Burgers = () => {
+  const [menuList, setMenuList] = useState<MenuList>([]);
+
+  useEffect(() => {
+    void (async () => {
+      try {
+        const data = await fetchMenuList();
+        setMenuList(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    })();
+  }, []);
+
+  return (
+    <div>
+      {menuList.map((menuItems, index) => (
+        <div key={index}>
+          {menuItems.map((menuItem) => (
+            <div key={menuItem.id}>
+              <img src={menuItem.imageUrl} alt={menuItem.menuName} />
+              <h4>{menuItem.menuName}</h4>
+              <p>{menuItem.price}원</p>
+              <p>수량: {menuItem.amount}</p>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Burgers;
