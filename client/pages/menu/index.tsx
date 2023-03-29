aba785ec06234e44955012e62ea0ea05844d7eeb
Jiseok Kim <86154028+p1ants@users.noreply.github.com>
2023-03-29 11:42:07 +0900

    [OUR-16] 카테고리 버튼 토글기능..

--- client/pages/menu/index.tsx
+++ client/pages/menu/index.tsx
@@ -1,26 +1,66 @@
-// 컴포넌트
+import React, { useState } from 'react';
+import Signature from '../../src/components/units/category/Signature';
+import Burgers from '../../src/components/units/category/Burgers';
+import SetMenu from '../../src/components/units/category/SetMenu';
+import SideMenu from '../../src/components/units/category/SideMenu';
+import Drink from '../../src/components/units/category/Drink';
+import Desserts from '../../src/components/units/category/Desserts';
+import CartList from '../../src/components/commons/layout/cart/CartList.container';
+import BackButton from '../../src/components/commons/button/BackButton';
 
-import Signature from "../../src/components/units/category/Signature";
-import Burgers from "../../src/components/units/category/Burgers";
-import SetMenu from "../../src/components/units/category/SetMenu";
-import SideMenu from "../../src/components/units/category/SideMenu";
-import Drink from "../../src/components/units/category/Drink";
-import Desserts from "../../src/components/units/category/Desserts";
-import CartList from "../../src/components/commons/layout/cart/CartList.container";
-import BackButton from "../../src/components/commons/button/BackButton";
+interface ShowCategoryState {
+  signature: boolean;
+  burgers: boolean;
+  setMenu: boolean;
+  sideMenu: boolean;
+  drink: boolean;
+  desserts: boolean;
+}
+
+type CategoryName = keyof ShowCategoryState;
 
 export default function MenuPage() {
+  const [showCategory, setShowCategory] = useState<ShowCategoryState>({
+    signature: false,
+    burgers: false,
+    setMenu: false,
+    sideMenu: false,
+    drink: false,
+    desserts: false,
+  });
+
+  const handleToggleCategory = (categoryName: CategoryName) => {
+    setShowCategory((prevState) => ({
+      ...prevState,
+      [categoryName]: !prevState[categoryName],
+    }));
+  };
+
   return (
     <>
       <BackButton />
       <div>MenuPage</div>
       <div>
-        <Signature />
-        <Burgers />
-        <SetMenu />
-        <SideMenu />
-        <Drink />
-        <Desserts />
+        <button onClick={() => handleToggleCategory('signature')}>
+          Signature
+        </button>
+        {showCategory.signature && <Signature />}
+        <button onClick={() => handleToggleCategory('burgers')}>Burgers</button>
+        {showCategory.burgers && <Burgers />}
+        <button onClick={() => handleToggleCategory('setMenu')}>
+          Set Menu
+        </button>
+        {showCategory.setMenu && <SetMenu />}
+        <button onClick={() => handleToggleCategory('sideMenu')}>
+          Side Menu
+        </button>
+        {showCategory.sideMenu && <SideMenu />}
+        <button onClick={() => handleToggleCategory('drink')}>Drink</button>
+        {showCategory.drink && <Drink />}
+        <button onClick={() => handleToggleCategory('desserts')}>
+          Desserts
+        </button>
+        {showCategory.desserts && <Desserts />}
       </div>
       <CartList />
     </>
