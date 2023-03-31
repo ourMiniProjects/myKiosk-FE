7f9a4249633a3bf3af5c5bbc1e41268183e10519
Jiseok Kim <86154028+p1ants@users.noreply.github.com>
2023-03-31 12:10:11 +0900

    [OUR-23] - 주문확인 페이지. 장바구니 데이터 들고오기 완료~^^

--- client/pages/order-check/index.tsx
+++ client/pages/order-check/index.tsx
@@ -1,17 +1,32 @@
-import { useRouter } from "next/router";
-import BackButton from "../../src/components/commons/button/BackButton";
+import { useRouter } from 'next/router';
+import BackButton from '../../src/components/commons/button/BackButton';
+import { useCart } from '../../src/components/commons/cart/CartContext';
 
 export default function CheckOrderPage() {
   const router = useRouter();
+  const { items } = useCart();
 
   const onClickMoveToOrderComplete = async () => {
-    await router.push("/order-complete");
+    await router.push('/order-complete');
+  };
+
+  const calculateTotalPrice = () => {
+    return items.reduce((acc, item) => acc + item.price * item.amount, 0);
   };
 
   return (
     <>
       <BackButton />
       <div>CheckOrderPage</div>
+      {items.map((item) => (
+        <div key={item.id}>
+          <div>{item.menuName}</div>
+          <div>{item.amount}</div>
+          <div> ${item.price}</div>
+          <div>: ${item.amount * item.price}</div>
+        </div>
+      ))}
+      <h2>Total: ${calculateTotalPrice()}</h2>
       <button onClick={onClickMoveToOrderComplete}>결제....</button>
     </>
   );
