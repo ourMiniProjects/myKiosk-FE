725e9fc1bb6af9ce73a08e104759855ecaa1c55d
yrk721 <yoorae0721@naver.com>
2023-04-03 13:07:56 +0900

    [OUR-31] 로그인 페이지 추가 / UI 구현 완료

--- client/pages/login/index.tsx
+++ client/pages/login/index.tsx
@@ -1,5 +1,13 @@
 import { useRouter } from "next/router";
 import BackButton from "../../src/components/commons/button/BackButton";
+import NumberPad from "../../src/components/commons/numberpad/NumberPad";
+import {
+  Container,
+  InputBox,
+  SelectCard,
+  SelectCardLogin,
+} from "../../src/components/units/login/Login.styles";
+import { Input } from "antd";
 
 export default function LoginPage() {
   const router = useRouter();
@@ -16,8 +24,19 @@ export default function LoginPage() {
     <>
       <div>LoginPage</div>
       <BackButton />
-      <button onClick={onClickMoveToSignup}>회원가입</button>
-      <button onClick={onClickMoveToMenu}>로그인</button>
+      <Container>
+        <SelectCard onClick={onClickMoveToSignup}>
+          <img src="/images/select-joinUs.png" />
+          간편 멤버십 가입하기
+        </SelectCard>
+        <SelectCardLogin onClick={onClickMoveToMenu}>
+          <img style={{ marginTop: "15px" }} src="/images/login-title.png" />
+          <InputBox>
+            <Input size="large" placeholder="전화번호를 입력해주세요." />
+          </InputBox>
+          <NumberPad />
+        </SelectCardLogin>
+      </Container>
     </>
   );
 }
---
+++ client/src/components/units/login/Login.styles.ts
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 800px;
+  /* background-color: #f6f6f6; */
+`;
+
+export const SelectCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 20%;
+  height: 55%;
+  padding: 20px;
+  margin: -100px 70px 0px 70px;
+  border: solid 1px #eaeaea9a;
+  border-radius: 30px;
+  box-shadow: 10px 10px 30px #bebebe, 10px 5px 10px #ffffff;
+  font-size: 20pt;
+  font-weight: 600;
+  color: #464646;
+
+  cursor: pointer;
+  :hover {
+    background-color: #f7ce5ccf;
+  }
+  img {
+    width: 70%;
+  }
+  :hover img {
+    -webkit-filter: grayscale(100%);
+    filter: grayscale(80%);
+    opacity: 20;
+  }
+`;
+
+export const SelectCardLogin = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 20%;
+  height: 55%;
+  padding: 20px;
+  margin: -100px 70px 0px 70px;
+  border: solid 1px #eaeaea9a;
+  border-radius: 30px;
+  box-shadow: 10px 10px 30px #bebebe, 10px 5px 10px #ffffff;
+  font-size: 20pt;
+  font-weight: 600;
+  color: #464646;
+
+  img {
+    width: 70%;
+  }
+`;
+
+export const InputBox = styled.div`
+  width: 85%;
+  margin: 20px 0px 20px 0px;
+`;
