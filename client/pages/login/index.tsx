c5259bfe03b9610373df72cd0111d8e52c7aaf12
Jiseok Kim <86154028+p1ants@users.noreply.github.com>
2023-04-03 19:20:38 +0900

    [OUR-32] 메뉴 wrap 이슈 해결함.

--- client/pages/login/index.tsx
+++ client/pages/login/index.tsx
@@ -1,23 +1,23 @@
-import { useRouter } from "next/router";
-import BackButton from "../../src/components/commons/button/BackButton";
-import NumberPad from "../../src/components/commons/numberpad/NumberPad";
+import { useRouter } from 'next/router';
+import BackButton from '../../src/components/commons/button/BackButton';
+import NumberPad from '../../src/components/commons/numberpad/NumberPad';
 import {
   Container,
   InputBox,
   SelectCard,
   SelectCardLogin,
-} from "../../src/components/units/login/Login.styles";
-import { Input } from "antd";
+} from '../../src/components/units/login/Login.styles';
+import { Input } from 'antd';
 
 export default function LoginPage() {
   const router = useRouter();
 
   const onClickMoveToSignup = async () => {
-    await router.push("/signup");
+    await router.push('/signup');
   };
 
   const onClickMoveToMenu = async () => {
-    await router.push("/menu");
+    await router.push('/menu');
   };
 
   return (
@@ -26,13 +26,13 @@ export default function LoginPage() {
       <BackButton />
       <Container>
         <SelectCard onClick={onClickMoveToSignup}>
-          <img src="/images/select-joinUs.png" />
+          <img src='/images/select-joinUs.png' />
           간편 멤버십 가입하기
         </SelectCard>
         <SelectCardLogin onClick={onClickMoveToMenu}>
-          <img style={{ marginTop: "15px" }} src="/images/login-title.png" />
+          <img style={{ marginTop: '15px' }} src='/images/login-title.png' />
           <InputBox>
-            <Input size="large" placeholder="전화번호를 입력해주세요." />
+            <Input size='large' placeholder='전화번호를 입력해주세요.' />
           </InputBox>
           <NumberPad />
         </SelectCardLogin>
--- client/src/components/commons/cart/CartList.presenter.tsx
+++ client/src/components/commons/cart/CartList.presenter.tsx
@@ -1,20 +1,20 @@
-import { useRouter } from "next/router";
-import { ICartListUIProps } from "./CartList.container";
-import { CartWrapper } from "./CartList.styles";
-import CartMenu from "./CartMenu";
+import { useRouter } from 'next/router';
+import { ICartListUIProps } from './CartList.container';
+import { CartTotal, CartWrapper } from './CartList.styles';
+import CartMenu from './CartMenu';
 
 export default function CartListUI(props: ICartListUIProps) {
   const router = useRouter();
 
   const onClickMoveToPay = () => {
-    void router.push("/order-check");
+    void router.push('/order-check');
   };
 
   return (
     <>
       <CartWrapper>
         <CartMenu />
-        <div onClick={onClickMoveToPay}>결제하기</div>
+        <CartTotal onClick={onClickMoveToPay}>결제하기</CartTotal>
       </CartWrapper>
     </>
   );
