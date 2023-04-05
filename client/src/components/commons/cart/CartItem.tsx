864d7de551c6a35528d395f4e9bcece64d7c45d1
yrk721 <yoorae0721@naver.com>
2023-04-06 04:47:44 +0900

    [OUR-35] CartItem 컴포넌트 분리

---
+++ client/src/components/commons/cart/CartItem.tsx
@@ -0,0 +1,38 @@
+import { useCart } from "./CartContext";
+import { MenuItem } from "../../units/category/MenuItem";
+import * as S from "./CartList.styles";
+
+interface CartItemProps {
+  item: MenuItem;
+  index: number;
+}
+
+const CartItem: React.FC<CartItemProps> = ({ item, index }) => {
+  const { incrementItem, decrementItem, removeItem } = useCart();
+
+  return (
+    <S.CartSelectedItems>
+      <S.MenuImage>
+        <img src={item.imageUrl} alt={item.menuName} />
+      </S.MenuImage>
+      <div key={index}>
+        <S.MenuName>
+          {item.menuName}
+          <S.DeleteIcon onClick={() => removeItem(item.id)} />
+        </S.MenuName>
+        <S.CountButtonWrapper>
+          <S.CountButton onClick={() => decrementItem(item.id)}>
+            -
+          </S.CountButton>
+          <S.AmoutBox>{item.amount}</S.AmoutBox>
+          <S.CountButton onClick={() => incrementItem(item.id)}>
+            +
+          </S.CountButton>
+          <S.MenuPrice>{item.price * item.amount}원</S.MenuPrice>
+        </S.CountButtonWrapper>
+      </div>
+    </S.CartSelectedItems>
+  );
+};
+
+export default CartItem;
