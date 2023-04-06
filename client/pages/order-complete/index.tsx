8e8ed4adbe0805628bd4bc96bf48fb7d512c9c18
yrk721 <yoorae0721@naver.com>
2023-04-06 19:34:38 +0900

    [OUR-33] 주문완료 페이지 - 카운트 UI 수정

--- client/pages/order-complete/index.tsx
+++ client/pages/order-complete/index.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import Lottie from 'react-lottie';
-import animationData from '../../src/components/commons/lotties/burger-preparing.json';
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import Lottie from "react-lottie";
+import animationData from "../../src/components/commons/lotties/burger-preparing.json";
 
 export default function OrderCompletePage() {
   const [timeoutId, setTimeoutId] = useState<null | NodeJS.Timeout>(null);
@@ -12,7 +12,7 @@ export default function OrderCompletePage() {
     autoplay: true,
     animationData: animationData,
     rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
+      preserveAspectRatio: "xMidYMid slice",
     },
   };
 
@@ -40,7 +40,7 @@ export default function OrderCompletePage() {
     }, 1000);
 
     const timeout = setTimeout(() => {
-      window.location.href = '/';
+      window.location.href = "/";
     }, 9000);
 
     return () => {
@@ -60,7 +60,9 @@ export default function OrderCompletePage() {
           <div>주문이 완료되었습니다!</div>
           <div>고객님의 주문번호는 00번 입니다.</div>
           <div>주문번호 호출 시, 카운터로 방문해주세요.</div>
-          <h3>{countdown}초 뒤 홈 화면으로 이동합니다.</h3>
+          <div>
+            <CountDown>{countdown}</CountDown>초 뒤 홈 화면으로 이동합니다.
+          </div>
         </TextBox>
       </Wrapper>
     </>
@@ -89,10 +91,19 @@ const TextBox = styled.div`
 `;
 
 const TextBox2 = styled.div`
-  font-family: 'myfont';
+  font-family: "myfont";
   font-size: 32pt;
   padding: 15px;
   margin-bottom: 10px;
+  font-weight: 700;
+  letter-spacing: 1.5pt;
+  color: #f39600;
+`;
+
+const CountDown = styled.span`
+  font-family: "myfont";
+  font-size: 20pt;
+  padding: 5px;
   font-weight: 700;
   letter-spacing: 1.5pt;
   color: #f39600;
