de48e9f9fc03f1bb364809cab1d394240e1d97e5
Jiseok Kim <86154028+p1ants@users.noreply.github.com>
2023-04-06 17:19:19 +0900

    [OUR-33] 주문완료 페이지 - 카운트 UI 표시

--- client/pages/order-complete/index.tsx
+++ client/pages/order-complete/index.tsx
@@ -1,17 +1,18 @@
-import { useEffect, useState } from "react";
-import styled from "styled-components";
-import Lottie from "react-lottie";
-import animationData from "../../src/components/commons/lotties/burger-preparing.json";
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import Lottie from 'react-lottie';
+import animationData from '../../src/components/commons/lotties/burger-preparing.json';
 
 export default function OrderCompletePage() {
   const [timeoutId, setTimeoutId] = useState<null | NodeJS.Timeout>(null);
+  const [countdown, setCountdown] = useState(9);
 
   const defaultOptions = {
     loop: true,
     autoplay: true,
     animationData: animationData,
     rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
+      preserveAspectRatio: 'xMidYMid slice',
     },
   };
 
@@ -21,15 +22,30 @@ export default function OrderCompletePage() {
   //   }, 9000);
   // });
 
+  // useEffect(() => {
+  //   const id = setTimeout(() => {
+  //     window.location.href = "/";
+  //   }, 9000);
+  //   setTimeoutId(id);
+  //   return () => {
+  //     if (timeoutId !== null) {
+  //       clearTimeout(timeoutId);
+  //     }
+  //   };
+  // }, []);
+
   useEffect(() => {
-    const id = setTimeout(() => {
-      window.location.href = "/";
+    const id = setInterval(() => {
+      setCountdown((prevCountdown) => prevCountdown - 1);
+    }, 1000);
+
+    const timeout = setTimeout(() => {
+      window.location.href = '/';
     }, 9000);
-    setTimeoutId(id);
+
     return () => {
-      if (timeoutId !== null) {
-        clearTimeout(timeoutId);
-      }
+      clearInterval(id);
+      clearTimeout(timeout);
     };
   }, []);
 
@@ -44,6 +60,7 @@ export default function OrderCompletePage() {
           <div>주문이 완료되었습니다!</div>
           <div>고객님의 주문번호는 00번 입니다.</div>
           <div>주문번호 호출 시, 카운터로 방문해주세요.</div>
+          <h3>{countdown}초 뒤 홈 화면으로 이동합니다.</h3>
         </TextBox>
       </Wrapper>
     </>
@@ -72,7 +89,7 @@ const TextBox = styled.div`
 `;
 
 const TextBox2 = styled.div`
-  font-family: "myfont";
+  font-family: 'myfont';
   font-size: 32pt;
   padding: 15px;
   margin-bottom: 10px;
