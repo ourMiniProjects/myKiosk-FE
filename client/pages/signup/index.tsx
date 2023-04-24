f20aba47e447807ebddf19ed8811e50c945975e3
Jiseok Kim <86154028+p1ants@users.noreply.github.com>
2023-04-24 18:21:30 +0900

    Merge branch 'dev_yoorae13' into dev_restore

--- client/pages/signup/index.tsx
+++ client/pages/signup/index.tsx
@@ -1,30 +1,30 @@
-import NumberPad from "../../src/components/commons/numberpad/NumberPad";
-import { useState } from "react";
-import { Container, ImageBox, SelectCardSignup } from "./signUp.styles";
+import NumberPad from '../../src/components/commons/numberpad/NumberPad';
+import { useState } from 'react';
+import { Container, ImageBox, SelectCardSignup } from './signUp.styles';
 
 export default function SignUpPage() {
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState('');
 
   const handleNumberPadClick = (value: string | KeyboardEvent): string => {
-    if (typeof value === "string") {
+    if (typeof value === 'string') {
       setInputValue((prev) => prev + value);
     }
-    return "";
+    return '';
   };
 
   return (
     <>
       <Container>
         <SelectCardSignup>
-          <img style={{ marginTop: "15px" }} src="/images/joinUs-title.png" />
+          <img style={{ marginTop: '15px' }} src='/images/joinUs-title.png' />
           <NumberPad
             onClick={handleNumberPadClick}
-            inputValue={""}
-            mode={"join"}
+            inputValue={''}
+            mode={'join'}
           />
         </SelectCardSignup>
         <ImageBox>
-          <img src="/images/welcome.png" />
+          <img src='/images/welcome.png' />
         </ImageBox>
       </Container>
     </>
